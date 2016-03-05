06e01db7386b62bed9ee40aa57c80f1f40299360
王国伟 <wanggw@zhong-ying.com>
2016-03-05 20:19:46 +0800

    提交测试

---
+++ src/test/java/com/snail/test/lettcode/MainTest.java
@@ -0,0 +1,12 @@
+package com.snail.test.lettcode;
+
+import java.net.SocketPermission;
+
+/**
+ * Created by wangguowei on 16-3-5.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
